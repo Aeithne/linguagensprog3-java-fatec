8eef174ddb8d0386652250d899a0a7d89fac6f67
Aeithne <40723680+Aeithne@users.noreply.github.com>
2025-09-02 21:21:13 -0300

    Update main.java

--- aula04/src/view/main.java
+++ aula04/src/view/main.java
@@ -12,7 +12,14 @@ public class Main {
 		
 		Figura figurinha = quadradinho;
 		System.out.println(String.format("[Upcasting] Área da Figura: %.2f cm2", figurinha.calcularArea()));
+		
+		// *** Exemplo de downcasting ***
+		
+		Figura figurita = new Quadrado();
 
+		Quadrado quadradito = (Quadrado) figurita;
+		quadradito.setLado(7);
+		System.out.println(String.format("[Upcasting] Área da Figura: %.2f cm2", figurita.calcularArea()));
 	}
 
 }
