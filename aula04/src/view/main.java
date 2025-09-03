f803f8eff25311760d159ccbde6224690a007e25
Aeithne <40723680+Aeithne@users.noreply.github.com>
2025-09-02 21:15:49 -0300

    Update main.java

--- aula04/src/view/main.java
+++ aula04/src/view/main.java
@@ -1,10 +1,17 @@
-
 package view;
+
+import business.Figura;
+import business.Quadrado;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// *** Exemplo de upcasting *** 
+		Quadrado quadradinho = new Quadrado();
+		quadradinho.setLado(5);
+		
+		Figura figurinha = quadradinho;
+		System.out.println(String.format("[Upcasting] Área da Figura: %.2f cm2", figurinha.calcularArea()));
 
 	}
 
