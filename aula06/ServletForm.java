928cea13b39f522b1bc14115d107ab570e3eacf7
Aeithne <40723680+Aeithne@users.noreply.github.com>
2025-10-14 21:33:57 -0300

    Update ServletForm.java

--- aula06/ServletForm.java
+++ aula06/ServletForm.java
@@ -1,6 +1,8 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,8 +28,25 @@ public class ServletForm extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html; charset=UTF-8");
+		try(PrintWriter out = response.getWriter()) {
+			out.println("<!DOCTYPE HTML>");
+			out.println("<html>");
+			out.println("<head><title>Meu Primeiro Servlet</title></head>");
+			out.println("<body>");
+			out.println("<h2>Meu Segundo Servlet</h2>");
+			out.println("<p>Sendo executado em" + request.getContextPath() + "</p>");
+			out.println("<p>ADS - Linguagens Programação 3</p>");
+			out.println("<form action=\"/Aula10-Parte2/ServletForm\" method=\"post\">");
+			out.println("<label for=\"fname\">Nome:</label><br>	");
+			out.println("<input type=\"text\" id=\"nome\" name=\"nome\"><br>	");
+			out.println("<label for=\"lname\">e-mail:</label><br>	");
+			out.println("<input type=\"email\" id=\"email\" name=\"email\"><br><br>");
+			out.println("<input type=\"submit\" value=\"Submit\">	");
+			out.println("</form>");
+			out.println("</body>");
+			out.println("</html>");
+		}
 	}
 
 	/**
