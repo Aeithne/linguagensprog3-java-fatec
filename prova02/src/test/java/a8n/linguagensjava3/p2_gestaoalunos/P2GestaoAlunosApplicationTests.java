0df59993ac35c2ffd0e54dc31410c9d9260fec7c
Aeithne <40723680+Aeithne@users.noreply.github.com>
2025-12-02 16:39:37 -0300

    css

---
+++ prova02/src/test/java/a8n/linguagensjava3/p2_gestaoalunos/P2GestaoAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package a8n.linguagensjava3.p2_gestaoalunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2GestaoAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
